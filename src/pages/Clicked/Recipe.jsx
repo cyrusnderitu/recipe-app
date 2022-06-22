2e32dbf3a9153d9aa4a91cf9df8c9d0858f780f5
cyrusnderitu <cyrusnderitu.k@gmail.com>
2022-06-22 14:26:30 +0300

    Recipe component fully functional

--- src/pages/Clicked/Recipe.jsx
+++ src/pages/Clicked/Recipe.jsx
@@ -8,6 +8,7 @@ function Recipe() {
   const params = useParams()
 
   const [recipeDetails, setRecipeDetails] = useState({})
+  const [activeButton, setActiveButton] = useState('meal')
 
   useEffect(()=>{
     getRecipe(params.name)
@@ -15,15 +16,51 @@ function Recipe() {
 
   const getRecipe = async (name)=> {
     const data = await fetch(`https://api.spoonacular.com/recipes/${name}/information?apiKey=${process.env.REACT_APP_API_KEY}`)
-    const res = data.json()
+    const res =  await data.json()
     setRecipeDetails(res)
   } 
 
+  console.log({recipeDetails})
   return (
-    <div>
-      <h2 className={classes.title}>
-        Todays, Recipe is for {recipeDetails.title} 
-      </h2>
+    <div className={classes.container}>
+      <div className={classes.display}>
+        <div className={classes.displayShow}>
+          <h3 className={classes.displayTitle}>{recipeDetails.title}</h3>
+          <div className={classes.displayImage}>
+            <img src={recipeDetails.image} alt={recipeDetails.title} className={classes.displayImg} />
+          </div>
+        </div>
+        <div className={classes.displayData}>
+          <div className={classes.buttons}>
+            <button className={classes.btn} onClick={()=> setActiveButton('meal')}>Meal Type</button>
+            <button className={classes.btn} onClick={()=> setActiveButton('ingredient')}>Ingredients</button>
+          </div>
+          {activeButton === 'meal' && (
+                      <ul className={classes.displayInfo}>
+                      <li>Dish Type: <span className={classes.subSection}>{recipeDetails.dishTypes}</span> </li>
+                      <li>Cuisine: <span  className={classes.subSection}>{recipeDetails.cuisines ? recipeDetails.cuisines : 'None'}</span> </li>
+                    </ul>
+          )}
+          {activeButton === 'ingredient' && (
+                    <ul className={classes.displayInfo}>
+                        {recipeDetails.extendedIngredients.map((ingredient)=>(
+                          <li key={ingredient.id} className={classes.displayList}>{ingredient.original}</li>
+                        ))}
+                    </ul>
+          )}
+
+        </div>
+      </div>
+      <div className={classes.data}>
+        <div className={classes.summary}>
+        <h3 className={classes.summaryTitle}>Meal Summary</h3>
+          <p dangerouslySetInnerHTML={{__html: recipeDetails.summary}}></p>
+        </div>
+        <div className={classes.instruction}>
+          <h3 className={classes.summaryTitle}>Instruction</h3>
+          <p dangerouslySetInnerHTML={{__html: recipeDetails.instructions}}></p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Clicked/styles.js
+++ src/pages/Clicked/styles.js
@@ -1,8 +1,66 @@
 import { makeStyles } from '@material-ui/core'
 
 export default makeStyles(()=> ({
-   title: {
-        fontSize: '15px',
-        marginTop: '2rem',
-   } 
+   container: {
+      width: '100%',
+      maxWidth: '1024px',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+   },
+   display: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(2, 1fr)',
+      marginTop: '3rem',
+      width: '100%',
+      // gap: '10rem'
+   },
+   displayImage: {
+      width: '500px',
+      height: 'auto',
+      borderRadius: '2rem',
+      overflow:'hidden'
+   },
+   displayImg: {
+      width: '100%',
+      objectFit: 'cover'
+   },
+   displayData: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+   },
+   displayInfo: {
+      marginTop: '2rem'
+   },
+   displayList: {
+      listStyle: 'circle',
+      marginLeft: '2rem',
+   },
+   buttons: {
+      display: 'flex',
+      marginTop: '2rem',
+      columnGap: '1rem'
+   },
+   btn:{
+      fontFamily: 'Poppins',
+      textTransform: 'uppercase',
+      fontSize: '1rem',
+      padding: '10px 20px',
+      cursor: 'pointer'
+   },
+   subSection: {
+      fontSize: '.936rem',
+      color: 'burlywood',
+      marginLeft: '10px'
+   },
+   summaryTitle: {
+      marginTop: '1.25rem',
+      fontFamily: 'Satisfy',
+      lineHeight: '1.6',
+      fontSize: '1.5rem'
+   }
+
 }))
